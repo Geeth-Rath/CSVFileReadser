55b803e4f5eb853474407058cadf5bbf30f93054
Geeth-Rath <geeth4862@gmail.com>
2022-01-26 21:58:02 +0530

    display the content of the the fille on the console

--- Program.cs
+++ Program.cs
@@ -5,8 +5,29 @@ namespace Program
 {
     class Program{
         static void Main(string []args ){
-                Console.WriteLine("Hello, World! to you all");
-                Console.WriteLine("Hello, World! to you all");
+                CSVFilereader();
+                Console.ReadLine();
         }
+
+        static void CSVFilereader(){
+            
+            var lines = File.ReadAllLines("people.csv");
+            var List = new List<People>();
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                var people = new People(){ Name = values[0], Company = values[1]};
+                List.Add(people);                
+            }
+            List.ForEach( x => Console.WriteLine($"{x.Name}\t{x.Company}")); 
+           
+        }
+    }
+
+    
+
+    class People{
+        public string Name {get; set;}
+        public string Company {get; set;}
     }
 }

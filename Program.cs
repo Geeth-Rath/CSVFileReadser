bb13c2a9fcab3718920e7b2ee96ba7e407d24b7d
Geeth-Rath <geeth4862@gmail.com>
2022-01-27 10:32:03 +0530

    creating the new collum

--- Program.cs
+++ Program.cs
@@ -1,27 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+// See https://aka.ms/new-console-template for more information
 
 
 namespace Program
 {
     class Program{
         static void Main(string []args ){
-                CSVFilereader();
+                AddNewColl();
                 Console.ReadLine();
         }
 
         static void CSVFilereader(){
-            
+
             var lines = File.ReadAllLines("people.csv");
             var List = new List<People>();
             foreach (var line in lines)
             {
                 var values = line.Split(',');
+               
                 var people = new People(){ Name = values[0], Company = values[1]};
                 List.Add(people);                
             }
             List.ForEach( x => Console.WriteLine($"{x.Name}\t{x.Company}")); 
            
         }
+
+         static void AddNewColl(){
+  
+            List<string> lines = File.ReadAllLines("people.csv").ToList();
+            StringBuilder sb = new StringBuilder("", lines.Count+1);
+            List<string> people = new List<string>();
+
+            // string[] arr = new String[5];
+            // int i =0;
+             foreach (var line in lines)
+            {     sb.Append(line);
+                var values = line.Split(',');
+                sb.Append(",");
+                 sb.Append(values[0] +"_"+ values[1] + "\n");
+                 
+              
+
+            }
+    
+          string test1 = sb.ToString();
+         File.WriteAllText("people.csv", test1); 
+
+        }
+
+
     }
 
     
@@ -29,5 +56,7 @@ namespace Program
     class People{
         public string Name {get; set;}
         public string Company {get; set;}
+
+        public string NameCompany {get; set;}
     }
 }

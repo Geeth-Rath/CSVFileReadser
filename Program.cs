c2c50224d0b40f2056976f9541734ed3b71c7b1c
Geeth-Rath <geeth4862@gmail.com>
2022-01-27 11:20:13 +0530

    remove unnesaary pieces

--- Program.cs
+++ Program.cs
@@ -4,58 +4,60 @@
 
 namespace Program
 {
-    class Program{
-        static void Main(string []args ){
-                AddNewColl();
-                Console.ReadLine();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNewColl();
+            Console.ReadLine();
         }
 
-        static void CSVFilereader(){
+        static void CSVFilereader()
+        {
 
             var lines = File.ReadAllLines("people.csv");
             var List = new List<People>();
             foreach (var line in lines)
             {
                 var values = line.Split(',');
-               
-                var people = new People(){ Name = values[0], Company = values[1]};
-                List.Add(people);                
+
+                var people = new People() { Name = values[0], Company = values[1] };
+                List.Add(people);
             }
-            List.ForEach( x => Console.WriteLine($"{x.Name}\t{x.Company}")); 
-           
+            List.ForEach(x => Console.WriteLine($"{x.Name}\t{x.Company}"));
+
         }
 
-         static void AddNewColl(){
-  
-            List<string> lines = File.ReadAllLines("people.csv").ToList();
-            StringBuilder sb = new StringBuilder("", lines.Count+1);
-            List<string> people = new List<string>();
+        static void AddNewColl()
+        {
 
-            // string[] arr = new String[5];
-            // int i =0;
-             foreach (var line in lines)
-            {     sb.Append(line);
+            List<string> lines = File.ReadAllLines("people.csv").ToList();
+            StringBuilder sb = new StringBuilder("", lines.Count + 1);
+           
+
+            foreach (var line in lines)
+            {
+                sb.Append(line);
                 var values = line.Split(',');
                 sb.Append(",");
-                 sb.Append(values[0] +"_"+ values[1] + "\n");
-                 
-              
+                sb.Append(values[0] + "_" + values[1] + "\n");
 
             }
-    
-          string test1 = sb.ToString();
-         File.WriteAllText("people.csv", test1); 
+
+            string test1 = sb.ToString();
+            File.WriteAllText("people.csv", test1);
 
         }
 
 
     }
 
-    
 
-    class People{
-        public string Name {get; set;}
-        public string Company {get; set;}
+
+    class People
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
 
     }
 }
